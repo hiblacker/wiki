0fa77cb89b47b2cffe345b9b7423bbe5d3e29bc2
hiblacker <hiblacker@qq.com>
2021-03-25 15:01:47 +0800

    新增linux命令手册

---
+++ docs/.vuepress/components/Linux.vue
@@ -0,0 +1,338 @@
+<template lang="pug">
+.linux
+    .list
+        .item(v-for='i in list', :key='i.name', :id='i.name')
+            h3(:id='i.name')
+                a.header-anchor(:href='`#${i.name}`') #
+                | {{ i.name }}
+            p.collect
+                span.command(v-for='j in i.keys', @click='go(j)') {{ j }}
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    name: 'linux',
+    data() {
+        return {
+            list: [
+                {
+                    name: '文件传输',
+                    keys: [
+                        'bye',
+                        'ftp',
+                        'ftpcount',
+                        'ftpshut',
+                        'ftpwho',
+                        'ncftp',
+                        'tftp',
+                        'uucico',
+                        'uucp',
+                        'uupick',
+                        'uuto',
+                        'scp',
+                    ],
+                },
+                {
+                    name: '备份压缩',
+                    keys: [
+                        'ar',
+                        'bunzip2',
+                        'bzip2',
+                        'bzip2recover',
+                        'compress',
+                        'cpio',
+                        'dump',
+                        'gunzip',
+                        'gzexe',
+                        'gzip',
+                        'lha',
+                        'restore',
+                        'tar',
+                        'unarj',
+                        'unzip',
+                        'zip',
+                        'zipinfo',
+                    ],
+                },
+                {
+                    name: '文件管理',
+                    keys: [
+                        'diff',
+                        'diffstat',
+                        'file',
+                        'find',
+                        'git',
+                        'gitview',
+                        'ln',
+                        'locate',
+                        'lsattr',
+                        'mattrib',
+                        'mc',
+                        'mcopy',
+                        'mdel',
+                        'mdir',
+                        'mktemp',
+                        'mmove',
+                        'mread',
+                        'mren',
+                        'mshowfat',
+                        'mtools',
+                        'mtoolstest',
+                        'mv',
+                        'od',
+                        'paste',
+                        'patch',
+                        'rcp',
+                        'rhmask',
+                        'rm',
+                        'slocate',
+                        'split',
+                        'tee',
+                        'tmpwatch',
+                        'touch',
+                        'umask',
+                        'whereis',
+                        'which',
+                        'cat',
+                        'chattr',
+                        'chgrp',
+                        'chmod',
+                        'chown',
+                        'cksum',
+                        'cmp',
+                        'cp',
+                        'cut',
+                        'indent',
+                    ],
+                },
+                {
+                    name: '磁盘管理',
+                    keys: [
+                        'cd',
+                        'df',
+                        'dirs',
+                        'du',
+                        'edquota',
+                        'eject',
+                        'lndir',
+                        'ls',
+                        'mcd',
+                        'mdeltree',
+                        'mdu',
+                        'mkdir',
+                        'mlabel',
+                        'mmd',
+                        'mmount',
+                        'mrd',
+                        'mzip',
+                        'pwd',
+                        'quota',
+                        'quotacheck',
+                        'quotaoff',
+                        'quotaon',
+                        'repquota',
+                        'rmdir',
+                        'rmt',
+                        'stat',
+                        'tree',
+                        'umount',
+                    ],
+                },
+                {
+                    name: '磁盘维护',
+                    keys: [
+                        'badblocks',
+                        'cfdisk',
+                        'dd',
+                        'e2fsck',
+                        'ext2ed',
+                        'fdisk',
+                        'fsck.ext2',
+                        'fsck',
+                        'fsck.minix',
+                        'fsconf',
+                        'hdparm',
+                        'losetup',
+                        'mbadblocks',
+                        'mformat',
+                        'mkbootdisk',
+                        'mkdosfs',
+                        'mke2fs',
+                        'mkfs.ext2',
+                        'mkfs',
+                        'mkfs.minix',
+                        'mkfs.msdos',
+                        'mkinitrd',
+                        'mkisofs',
+                        'mkswap',
+                        'mpartition',
+                        'sfdisk',
+                        'swapoff',
+                        'swapon',
+                        'symlinks',
+                        'sync',
+                    ],
+                },
+                {
+                    name: '文本处理',
+                    keys: [
+                        'awk',
+                        'col',
+                        'colrm',
+                        'comm',
+                        'csplit',
+                        'ed',
+                        'egrep',
+                        'ex',
+                        'fgrep',
+                        'fmt',
+                        'fold',
+                        'grep',
+                        'ispell',
+                        'jed',
+                        'joe',
+                        'join',
+                        'look',
+                        'mtype',
+                        'pico',
+                        'rgrep',
+                        'sed',
+                        'sort',
+                        'spell',
+                        'tr',
+                        'uniq',
+                        'vi',
+                        'wc',
+                    ],
+                },
+                {
+                    name: '网络通讯',
+                    keys: [
+                        'dip',
+                        'getty',
+                        'mingetty',
+                        'ppp-off',
+                        'telnet',
+                        'uulog',
+                        'uustat',
+                        'uux',
+                        'cu',
+                        'dnsconf',
+                        'efax',
+                        'httpd',
+                        'ip',
+                        'ifconfig',
+                        'mesg',
+                        'minicom',
+                        'nc',
+                        'netconf',
+                        'netconfig',
+                        'netstat',
+                        'ping',
+                        'pppstats',
+                        'samba',
+                        'setserial',
+                        'shapecfg(shaper configuration)',
+                        'smbd(samba daemon)',
+                        'statserial(status ofserial port)',
+                        'talk',
+                        'tcpdump',
+                        'testparm(test parameter)',
+                        'traceroute',
+                        'tty(teletypewriter)',
+                        'uuname',
+                        'wall(write all)',
+                        'write',
+                        'ytalk',
+                        'arpwatch',
+                        'apachectl',
+                        'smbclient(samba client)',
+                        'pppsetup',
+                    ],
+                },
+                {
+                    name: '设备管理',
+                    keys: ['dumpkeys', 'loadkeys', 'MAKEDEV', 'rdev', 'setleds'],
+                },
+                {
+                    name: '电子邮件与新闻组',
+                    keys: [
+                        'archive',
+                        'ctlinnd',
+                        'elm',
+                        'getlist',
+                        'inncheck',
+                        'mail',
+                        'mailconf',
+                        'mailq',
+                        'messages',
+                        'metamail',
+                        'mutt',
+                        'nntpget',
+                        'pine',
+                        'slrn',
+                        'X WINDOWS SYSTEM',
+                        'reconfig',
+                        'startx(start X Window)',
+                        'Xconfigurator',
+                        'XF86Setup',
+                        'xlsatoms',
+                        'xlsclients',
+                        'xlsfonts',
+                    ],
+                },
+                {
+                    name: '其他命令',
+                    keys: ['yes'],
+                },
+            ],
+            keyMap: null,
+        }
+    },
+    methods: {
+        go(command) {
+            const c = command.split('(')[0]
+            const url = `https://wangchujiang.com/linux-command/c${this.keyMap[c].p}.html`
+            window.open(url)
+        },
+    },
+    mounted() {
+        const body = {
+            url: 'https://unpkg.com/linux-command/dist/data.json',
+        }
+        axios(body).then(({ data }) => {
+            this.keyMap = data
+        })
+    },
+}
+</script>
+
+<style scoped lang="stylus">
+.linux {
+    .list {
+        .item {
+            line-height 1.7
+            h3 {
+                margin-top -3.1rem
+                padding-top 4.6rem
+                margin-bottom 0
+            }
+            .collect {
+                word-break break-word
+            }
+            .command {
+                display inline-block
+                font-weight 500
+                color #3eaf7c
+                text-decoration none
+                cursor pointer
+                margin-right 10px
+                &:hover {
+                    text-decoration underline
+                }
+            }
+        }
+    }
+}
+</style>
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,62 +1,64 @@
 module.exports = {
     title: "Superwen's wiki",
     description: "Superwen's wiki",
-    head: [["link", { rel: "icon", href: "/ico.ico" }]],
+    head: [['link', { rel: 'icon', href: '/ico.ico' }]],
     port: 6688,
     markdown: {
         lineNumbers: true,
-        extendMarkdown: (md) => {
+        extendMarkdown: md => {
             // 复制代码块
-            md.use(require("markdown-it-copy"), {
-                btnText: "复制代码", // 'copy' | button text
-                failText: "fail", // 'copy fail' | copy-fail text
-                successText: "ok", // 'copy success' | copy-success text
-            });
+            md.use(require('markdown-it-copy'), {
+                btnText: '复制代码', // 'copy' | button text
+                failText: 'fail', // 'copy fail' | copy-fail text
+                successText: 'ok', // 'copy success' | copy-success text
+            })
         },
     },
     themeConfig: {
         nav: [
-            { text: "代码片段", link: "/snippets/js/util" },
-            { text: "运维", link: "/DevOps/" },
+            { text: '代码片段', link: '/snippets/js/util' },
+            { text: '运维', link: '/DevOps/' },
             {
-                text: "工具",
-                ariaLabel: "Language Menu",
+                text: '工具',
+                ariaLabel: 'Language Menu',
                 items: [
-                    { text: "博客搭建", link: "/tools/blog/" },
-                    { text: "工作流", link: "/tools/workflow/" },
+                    { text: '博客搭建', link: '/tools/blog/' },
+                    { text: '工作流', link: '/tools/workflow/' },
+                    { text: 'Git', link: '/tools/git/' },
                 ],
             },
             {
-                text: "文档",
-                ariaLabel: "Language Menu",
-                items: [
-                    { text: "Webpack", link: "https://books.superwen.cn/webpack/" },
-                ],
+                text: '文档',
+                ariaLabel: 'Language Menu',
+                items: [{ text: 'Webpack', link: 'https://books.superwen.cn/webpack/' }],
             },
-            { text: "我的博客", link: "https://superwen.cn" },
+            { text: '我的博客', link: 'https://superwen.cn' },
         ],
         sidebar: {
-            "/DevOps/": ["ssh", "npm-scripts-deploy", "docker-deploy"],
-            "/snippets/": [
+            '/DevOps/': [
+                'ssh',
+                'npm-scripts-deploy',
+                'linux',
                 {
-                    title: "JavaScript", // 必要的
-                    path: "/snippets/js/util", // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-                    // collapsable: false, // 可选的, 默认值是 true,
+                    title: 'DevOps', // 必要的
+                    path: '/DevOps/docker/why-docker', // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+                    collapsable: false, // 可选的, 默认值是 true,
                     sidebarDepth: 2, // 可选的, 默认值是 1
-                    children: [
-                        {
-                            title: "JS utils",
-                            path: "js/util",
-                        },
-                        {
-                            title: "DOM utils",
-                            path: "js/dom-util",
-                        },
-                    ],
+                    children: ['docker/why-docker', 'docker/docker-deploy'],
                 },
             ],
-            "/tools/blog/": ["/tools/blog/VuePress"],
-            "/tools/workflow/": ["/tools/workflow/image-uploader"],
+            '/snippets/': [
+                {
+                    title: 'JavaScript', // 必要的
+                    path: '/snippets/js/util', // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+                    // collapsable: false, // 可选的, 默认值是 true,
+                    sidebarDepth: 2, // 可选的, 默认值是 1
+                    children: ['js/util', 'js/dom-util', 'js/canvas'],
+                },
+            ],
+            '/tools/blog/': ['/tools/blog/VuePress'],
+            '/tools/workflow/': ['/tools/workflow/image-uploader'],
+            '/tools/git/': ['/tools/git/git'],
         },
     },
-};
+}
